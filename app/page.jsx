108000e6addd9abb5360c2dbea862dfbda8c759f
Oghenevwede Louis Umukoro <168831499+unsurep@users.noreply.github.com>
2025-03-11 10:01:10 +0100

    section2 update

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,10 @@ import Image from 'next/image'
 import React, { useEffect } from 'react'
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 import { motion } from "motion/react"
+import { IoHomeOutline } from "react-icons/io5";
+import { IoFlowerOutline } from "react-icons/io5";
+import { CgStyle } from "react-icons/cg";
+import { MdMoreTime } from "react-icons/md";
 
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
@@ -38,10 +42,10 @@ const Home = () => {
     <div>
 
       {/* Section 1 */}
-      <section id='section1' className='h-[85vh] md:h-screen text-white bg-cover bg-center bg-no-repeat flex items-cente  px-[1rem] md:px-[3rem]' style={{ backgroundImage: "url('/image/hbg.png')" }}>
+      <section id='section1' className='h-[85vh] md:h-screen text-white bg-cover bg-center bg-no-repeat flex items-cente  px-[1rem] md:px-[3rem] font-poppins' style={{ backgroundImage: "url('/image/hbg.png')" }}>
         <div  className='flex flex-col gap-4 text-center md:justify-center items-center pt-10'>
           <h1 data-aos="zoom-in"
-          data-aos-duration="1000" className='text-3xl md:text-5xl font-bold tracking-[5px] md:pt-'>Discover Your Dream Home <span className='text-red-500 tracking-wide font-extrabold'>{text} <Cursor cursorColor='red' /></span></h1>
+          data-aos-duration="1000" className='text-3xl md:text-5xl font-bold tracking-[5px] md:pt- '>Discover Your Dream Home <span className='text-red-500 tracking-wide font-extrabold'>{text} <Cursor cursorColor='red' /></span></h1>
 
           <p data-aos="zoom-in"
           data-aos-duration="1500"  className=' text-white text-xl leading-relaxed tracking-wide opacity-90  md:pt-8'>Tehiljem Homes envisions a world where luxury meets affordability,  making exceptional living spaces accessible to all. Our mission  is to provide elegantly designed, high-quality homes that enhance  the lives of our clients and their families, ensuring comfort, style, and  long-term value. </p>
@@ -62,7 +66,7 @@ const Home = () => {
 
 
       {/* Section 2 */}
-      <section id='section2' className='h-screen pt-16 flex gap-8 px-[1rem] md:px-[3rem] '>
+      <section id='section2' className='h-screen pt-16 lg:flex gap-8 px-[1rem] md:px-[3rem] font-poppins'>
         {/* div A */}
         <div>
           <Image src='/image/chair.png' width={700} height={700} alt='image' className='rounded'/>
@@ -72,23 +76,27 @@ const Home = () => {
 
         {/* div B */}
         <div className='flex flex-col gap-5'>
-          <h3 className='text-xl'>About Us</h3>
-          <h1 className='text-4xl font-bold'>The Leading Real Estate <br /> Marketplace.</h1>
-          <p className='text-base'>Over 39,000 people work for us in more than 70 countries all over the This breadth of global coverage, combined with specialist services</p>
+          <h3 className='text-xl hvr-bubble-float-bottom px-3 py-1 bg-orange-200/60 text-orange-400 rounded w-fit'>About Us</h3>
+          <h1 className='text-4xl font-bold tracking-[5px]'>The Leading Real Estate <br /> Marketplace.</h1>
+          <p className='text-base tracking-wide'>We started with a passionate team, focusing on residential <br /> developments in Abuja. As our vision grew, so did our reach, <br /> expanding into Edo State and diversifying into commercial <br /> projects. In 2015, we introduced TJ Homes, a brand dedicated <br /> to delivering excellence and affordability in real estate.
 
-          <div className='grid grid-cols-2'>
-            <p>Smart Home Design</p>
-            <p>Beautiful Scene Around</p>
-            <p>Exceptional Lifestyle</p>
-            <p>Complete 24/7 Security</p>
+          Today, <br /> TEHILJEM Nig. Ltd stands as a trusted name in Nigeria’s real <br /> estate sector, renowned for innovative housing solutions and <br /> a customer-first approach. Our commitment remains <br /> unwavering—building homes, shaping communities, and <br /> redefining excellence in real estate development.</p>
+
+          <div className='grid grid-cols-2 gap-6'>
+            <p className='flex items-center gap-3 '><span className='bg-orange-400 tracking-wide p-3 rounded-full text-white font-bold text-2xl flip-vertical-left'><IoHomeOutline /></span>Smart Home Design</p>
+            <p className='flex items-center gap-3'><span  className='bg-orange-400 tracking-wide p-3 rounded-full text-white font-bold text-2xl flip-vertical-left'><IoFlowerOutline /></span>Beautiful Enviroment</p>
+            <p className='flex items-center gap-3'><span  className='bg-orange-400 tracking-wide p-3 rounded-full text-white font-bold text-2xl flip-vertical-left'><CgStyle /></span>Exceptional Lifestyle</p>
+            <p className='flex items-center gap-3'><span  className='bg-orange-400 v p-3 rounded-full text-white font-bold text-2xl flip-vertical-left'><MdMoreTime /></span>Complete 24/7 Security</p>
           </div>
 
           <div>
-            <p>"Enimad minim veniam quis nostrud exercitation
+            <p className='tracking-wide'>"Enimad minim veniam quis nostrud exercitation
             llamco laboris. Lorem ipsum dolor sit amet"</p>
           </div>
 
-          <button>Our Services</button>
+         <div className='flex items-center justify-center w-full'>
+          <button className='bg-orange-500 text-white font-semibold px-6 py-3 rounded-md shadow-md hover:bg-orange-600 hover:scale-105 transition-transform duration-300 focus:ring-4 focus:ring-orange-300 cursor-pointer  '>Our Services</button>
+         </div>
         </div>
       </section>
 
